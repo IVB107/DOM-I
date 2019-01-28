416b6c04e1ce2013867952ff06acdaed328a36f1
IVB107 <ianvb107@gmail.com>
2019-01-28 15:31:20 -0600

    First commit. Completed Nav and CTA sections

--- js/index.js
+++ js/index.js
@@ -37,6 +37,21 @@ const siteContent = {
   },
 };
 
+// Header
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+// Nav Links
+document.querySelectorAll("nav a").forEach((link, index) => {
+  link.textContent = siteContent["nav"][`nav-item-${index + 1}`];
+})
+
+// CTA
+
+document.querySelector(".cta-text h1").textContent = siteContent["cta"]["h1"];
+document.querySelector(".cta-text button").textContent = siteContent["cta"]["button"];
+document.getElementById("cta-img").src = siteContent["cta"]["img-src"];
+
+
+// Main Content
